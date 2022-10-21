004fe017a2ee1425833677e922bb12d4edef262e
Jose Franklim Fernandez V <jhosepfernan2001@gmail.com>
2022-10-21 18:05:49 -0400

    cambio de iconos

--- js/Alerts2.js
+++ js/Alerts2.js
@@ -1,8 +1,22 @@
 swal({
-    title: "Registra tu pruducto!",
-    text: "No te olvides llenar todos lo campos!!!",
-    icon: "info",
-    button: "Aceptar!",
+  title: "Registra tu pruducto!",
+  text: "No te olvides llenar todos lo campos!!!",
+  icon: "info",
+  button: "Aceptar!",
 
-  });
+});
 
+function validar() {
+
+  //Almacenamos los valores
+  nombre = $('#cantidad').val();
+
+  //Comprobamos la longitud de caracteres
+  if (nombre.length < 2) {
+    return true;
+  }
+  else {
+    alert('Maximo 2 caracteres');
+    return false;
+  }
+}
