fc34f5140c033436d535fec1f45d2000dfe30c41
Kevin Fernandez <cokodx@gmail.com>
2022-10-29 17:01:27 -0400

    crear cuenta

--- config/env.php
+++ config/env.php
@@ -1,6 +1,6 @@
 <?php
     /** URL raiz del proyecto */
-    define('RUTA', 'http://localhost/proyecto/EatSoon/');
+    define('RUTA', 'http://localhost/ProyectoEatSoon/EatSoon');
 
     $db_config = [
         'db_user' => 'root',
