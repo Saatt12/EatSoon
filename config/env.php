1d1b69cd10bc3156c3d642084e0e0bffca266af7
ensy <010lobo010@gmail.com>
2022-11-05 08:55:49 -0400

    configuracion

--- config/env.php
+++ config/env.php
@@ -1,10 +1,8 @@
 <?php
     /** URL raiz del proyecto */
-<<<<<<< HEAD
+
     define('RUTA', 'http://localhost/proyecto/EatSoon');
-=======
-    define('RUTA', 'http://localhost/ProyectoEatSoon\EatSoon');
->>>>>>> fa7c5caa29f04b96ad78cc3301722dab01c8839d
+
 
     $db_config = [
         'db_user' => 'root',
