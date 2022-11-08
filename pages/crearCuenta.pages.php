cd8fe18551a7e0be552fb3687dc9b318e7c9ff37
Kevin Fernandez <cokodx@gmail.com>
2022-11-08 15:34:44 -0400

    correcciones crearCuenta1

--- pages/crearCuenta.pages.php
+++ pages/crearCuenta.pages.php
@@ -18,7 +18,7 @@
                 <div class="container align-self-center p-6">
                     <h1 class="font-weight-bold mb-3"><img src="assets/img/user3.png" width="15%" /> Crear Cuenta</h1>
                     
-                    <p class="text-muted mb-3">Ingrese la siguiente información para registrarte:</p>
+                    <p class="text-muted mb-3">Ingrese la siguiente información para registrarse:</p>
                     <form id = "registroCuenta" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="form-horizontal" style="margin:0 auto" >                        
                      
                      <table class="formularioTabla">
@@ -26,16 +26,16 @@
                             
                         <tr>
                             <td><i class="bi bi-person " ></i> &nbsp Nombres:<span class="text-danger">*</span></td>
-                            <td><input class="inputAlinear" class="form-control"  id="nombre" name="nombre" placeholder= "Ingrese sus nombres" oninput="this.value = this.value.replace(/[^a-z A-Z]/,'')" required minlength ="3" maxlength ="15" ></td>
+                            <td><input class="inputAlinear" class="form-control"  id="nombre" name="nombre" placeholder= "Ingrese sus nombres" oninput="this.value = this.value.replace(/[^a-z A-Z ñ-Ñ]/,'')" required minlength ="3" maxlength ="15" ></td>
                         
                         </tr>
                            
                         <tr>
                             <td><i class="bi bi-person" ></i> &nbsp Apellidos:<span class="text-danger">*</span></td>
-                            <td><input class="inputAlinear" id="apellido" name="apellido" placeholder= "Ingrese sus apellidos" oninput="this.value = this.value.replace(/[^a-z A-Z]/,'')" required minlength ="3" maxlength ="15" ></td>
+                            <td><input class="inputAlinear" id="apellido" name="apellido" placeholder= "Ingrese sus apellidos" oninput="this.value = this.value.replace(/[^a-z A-Z ñ-Ñ]/,'')" required minlength ="3" maxlength ="15" ></td>
                         </tr>
                         <tr class="filaTabla">
-                            <td><i class="bi bi-credit-card-2-front"></i> &nbsp C.i:<span class="number-danger">*</span></td>
+                            <td><i class="bi bi-credit-card-2-front"></i> &nbsp C.i:<span class="text-danger">*</span></td>
                             <td><input class="inputAlinear" id="ci" name="ci" type="number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"  placeholder= "Ingrese C.I." pattern="^[0-9]+"  minlength ="7" maxlength ="10" min="1111111" max="9999999999"  required   ></td>
                         </tr>
                         <tr>
@@ -43,8 +43,8 @@
                             <td><input class="inputAlinear" id="direccion" name="direccion" placeholder= "Ingrese dirección" required minlength ="10" maxlength ="70" ></td>
                         </tr>
                         <tr>
-                            <td><i class="bi bi-phone"></i> &nbsp Telefono:<span class="number-danger">*</span></td>
-                            <td><input class="inputAlinear" id="telefono" name="telefono" type="number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"  placeholder= "Ingrese número de telefono"   required  min="44444444" max="79999999" pattern="^[0-9]+"  minlength ="7" maxlength ="8"></td>
+                            <td><i class="bi bi-phone"></i> &nbsp Teléfono:<span class="text-danger">*</span></td>
+                            <td><input class="inputAlinear" id="telefono" name="telefono" type="number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"  placeholder= "Ingrese número de teléfono"   required  min="44444444" max="79999999" pattern="^[0-9]+"  minlength ="7" maxlength ="8"></td>
                         </tr>
                         <tr>
                             <td><i class="bi bi-envelope"></i> &nbsp Email:<span class="text-danger">*</span></td>
@@ -70,7 +70,7 @@
                         <div class="form-group mb-2">
                             <button type="button" id ="bottonDescartarr" class="btn btn-info mt-3 btn-setupC btn-space"  onclick="fntdescartarCuenta(1)" >Cancelar </button>
                     
-                            <button class="btn btn-warning mt-3 btn-setupA btn-space"  >Crear </button>
+                            <button class="btn btn-warning mt-3 btn-setupA btn-space"  >Aceptar </button>
                         </div>
                         <!--<small class="d-inline-block text-muted mt-3">Gracias y Bienvenido a Eat-Soon!!! </small>
                     
