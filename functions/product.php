3472267c1e6b7bfae029e9450c251d0b4b6a172e
Sergio A <saat.122090@gmail.com>
2022-11-10 23:52:31 -0400

    bug de paginacion solucionado

--- functions/product.php
+++ functions/product.php
@@ -7,32 +7,53 @@
         return count($query->fetchAll()) > 0;
     }
 
-    /*function getProducts($con){
-        $cantPagina = 8;
+    function redireccionar($products,$ruta){
+        $productSize = count($products);
+        if(isset($_GET['page'])){
+            if($productSize == 0 && (int)($_GET['page'])>1 ){
+                header("location: $ruta/index.php?page=1");
+            } 
+        }
+    }
+
+    function getCantPaginas($con, $actual, $limite){
         
-        $query = $con->prepare("SELECT * FROM producto WHERE fecha_publicacion ORDER BY fecha_publicacion desc LIMIT 0,$cantPagina ");
-        $query->execute();
-        $aux = $query->rowCount();
-        $page = $aux/$cantPagina;
-        $page = ceil($page);
-        $GLOBALS['cant'] = $page;
-        return $query->fetchAll();
         
-    }*/
+        $query = $con->query("SELECT FOUND_ROWS() AS cantidad");
+        $cantidad = $query->fetch()['cantidad'];
+        return ceil($cantidad/$limite);
+        
+    }
 
 
 function getProductoPage($con,$actual,$limite){
-        $cantPagina = 8;
+       
         
-        $query = $con->prepare("SELECT * FROM producto WHERE fecha_publicacion ORDER BY fecha_publicacion desc LIMIT $actual,$limite ");
+        $query = $con->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM producto WHERE fecha_publicacion ORDER BY fecha_publicacion desc LIMIT $actual,$limite ");
         $query->execute();
         $aux = $query->rowCount();
-        $page = $aux/$cantPagina;
+        $page = $aux/$limite;
         $page = ceil($page);
         $GLOBALS['cant'] = $page;
         return $query->fetchAll();
+
+
         
     }
+
+    function limitarProductos($con){
+        $query = $con->prepare("SELECT * FROM producto");
+        $query->execute();
+        $numeroProductos = count($query->fetchAll());
+        //var_dump($numeroProductos);
+        if($numeroProductos > 60){
+            $query = $con->prepare("DELETE FROM producto WHERE id_producto = (SELECT MIN(id_producto) FROM producto)");
+            $query->execute();
+        }
+    }
+
+
+
 
     function createProduct($con, $data, $folder_save, $uploadFile){
         if(!existProduct($con, $data['nombre_producto'])){ //si no existe, se registra
