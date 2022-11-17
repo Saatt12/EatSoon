f2cc29374e7440d58cfe30e58991ab8a987a5ff9
EDelgadillo <104334841+EDelgadillo26@users.noreply.github.com>
2022-11-16 21:42:26 -0400

    Permite espacios en los nombres

--- pages/crearCuenta.pages.php
+++ pages/crearCuenta.pages.php
@@ -32,14 +32,14 @@
                         <tr>
                             <td><i class="bi bi-person " ></i> &nbsp Nombres:<span class="text-danger">*</span></td>
                             <td><input class="inputAlinear" class="form-control"  id="nombre" name="nombre" placeholder= "Ingrese sus nombres" 
-                            oninput="this.value = this.value.replace(/[^\a-\z\A-\Z\ñ\Ñ]/g,'')" required minlength ="3" maxlength ="15" ></td>
+                            oninput="this.value = this.value.replace(/[^\a-\z\A-\Z\ñ\Ñ ]/g,'')" required minlength ="3" maxlength ="15" ></td>
                         
                         </tr>
                            
                         <tr>
                             <td><i class="bi bi-person" ></i> &nbsp Apellidos:<span class="text-danger">*</span></td>
                             <td><input type="text" class="inputAlinear" id="apellido" name="apellido" placeholder= "Ingrese sus apellidos" 
-                            oninput="this.value = this.value.replace(/[^\a-\z\A-\Z\ñ\Ñ]/g,'')"  minlength ="3" maxlength ="15" required></td>
+                            oninput="this.value = this.value.replace(/[^\a-\z\A-\Z\ñ\Ñ ]/g,'')"  minlength ="3" maxlength ="15" required></td>
                         </tr>
                         <tr class="filaTabla">
                             <td><i class="bi bi-credit-card-2-front"></i> &nbsp C.i:<span class="text-danger">*</span></td>
