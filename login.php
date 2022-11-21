17dd8c5ac2db52a6b3e92e37503eec5af1e40582
Adalid <adalidarnez77@gmail.com>
2022-11-21 01:51:02 -0400

    lectura de variables de formulario login

--- login.php
+++ login.php
@@ -1,49 +1,25 @@
 <?php
 
-require './config/env.php';
-require './config/conexion.php';
-require './functions/loginf.php'; 
+    require './config/env.php';
+    require './config/conexion.php';
+    require './functions/loginf.php';
+    require './functions/file.php';
 
-$con = conexion($db_config);
-    
-/** Verificar si se envio por el metodo POST */
-if($_SERVER['REQUEST_METHOD'] == 'POST'){    
-    $data = [
-        'nombre' => $_POST['nombre'],
-        'apellido' => $_POST['apellido'],
-        'ci' => $_POST['ci'],
-        'direccion' => $_POST['direccion'],
-        'telefono' => $_POST['telefono'],
-        'email' => $_POST['email'],
-        'password' => $_POST['password'],
-        'Cpassword' => $_POST['Cpassword']
+    $con = conexion($db_config);
 
-    ];
-    if($data['password']==$data['Cpassword']){
-        $create = createUser($con, $data);
+    /** Verificar si se envio por el metodo POST */
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){    
+        $data = [
+            'usuario' => $_POST['usuario'],
+            'password' => $_POST['password']
+        ];
 
-    }else{
-        //para mostrar mensaje no crear cuenta
-      
-        echo "<script type='text/javascript'>";
-        echo "mensajeexito(1)"; 
-        echo "</script> ";
-      
+        loginUser($con, $data);
     }
-
-
-if(!$create){
-     echo "La cuenta ingresado ya existe";
-     
-}
-
-    header('Location: login.php');
-}
-        
+    
     
     $title = "Login"; // Nombre del title
 
     $page = './pages/login.page.php';  // Nombre y ruta de la pagina
     require './templates/login.template.php'; // Require template
-
 ?>
