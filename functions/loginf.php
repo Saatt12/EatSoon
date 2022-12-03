15ddb4220786e679ce3d5ee104a3bdce055e42e2
Adalid <adalidarnez77@gmail.com>
2022-12-02 22:44:23 -0400

    aniadiendo sms de error

--- functions/loginf.php
+++ functions/loginf.php
@@ -1,7 +1,35 @@
 
+<
 <?php 
 
-    //limpiado
+    function createUser($con, $data){        
+          if(no_repetirUsuario($con, $data)){  
+            $query = $con->prepare(
+                'INSERT INTO usuario (ci, nombre, apellido, direccion, telefono, correo, contraseña)
+                            VALUES (:ci, :nombre, :apellido, :direccion, :telefono, :email, :pass)'
+            );
+
+
+            $query->execute([
+                ':nombre' => $data['nombre'],
+                ':apellido' => $data['apellido'],
+                ':ci' => $data['ci'],
+                ':direccion' => $data['direccion'],
+                ':telefono' => $data['telefono'], 
+                ':email' => $data['email'],
+                ':pass' => $data['password']
+            ]);
+
+// Para direccionar automaticamente al inicio despues de crear una cuenta
+          header("Location: index_usuario_creado.php");
+              die();
+
+
+            return true;
+    }else{
+        return false;
+    }
+}
 
     function no_repetirUsuario($con, $data){
         $ci = $data["ci"];
