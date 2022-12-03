dc9271bc7cf3af80bee55d8648d56ee8906d7968
Sergio <saat.122090@gmail.com>
2022-12-03 15:36:01 -0400

    template carrito

--- templates/carrito.template.php
+++ templates/carrito.template.php
@@ -1,5 +1,7 @@
-<!-- modal carrito, pero mejor si es offcanvas -->
-<div class="modal fade" id="demo" >
+<?php
+$total=0;
+?>
+<div class="modal fade" id="demo">
     <div class="modal-dialog modal-lg modal-dialog-scrollable">
         <div class="modal-content bg-dark">
             <div class="modal-header">
@@ -18,48 +20,70 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>(imagen)Leche Pil</td>
-                            <td>cantidad</td>
-                            <td>- +</td>
-                            <td>total en carrito</td>
-                            <td><a href="#" class="bi bi-trash"></a></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>(imagen)Mermelada del Valle</td>
-                            <td>cantidad</td>
-                            <td>- +</td>
-                            <td>total en carrito</td>
-                            <td><a href="#" class="bi bi-trash"></a></td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                            <td>(imagen)atun Van Camps</td>
-                            <td>cantidad</td>
-                            <td>- +</td>
-                            <td>total en carrito</td>
-                            <td><a href="#" class="bi bi-trash"></a></td>
-                        </tr>
+                        <?php foreach ($enCarrito as $carito) : ?>
+                            <tr>
+                                <th scope="row"><?php echo ($carito['carrito_id']); ?></th>
+                                <td><img src="<?php echo RUTA . '/storage/' . $carito['imagen'] ?>" width="30" /> <?php echo ($carito['nombre_producto']); ?></td>
+                                <td><?php echo ($carito['cantidad']); ?></td>
+                                <td>
+                                    <div style="display: inline-flex;">
+                                        <form action="carrito.php" method="post">
+                                            <input type="hidden" name="action" value="menos" />
+                                            <input type="hidden" name="page" value="<?php echo ($_GET['page']) ?>" />
+                                            <input type="hidden" name="carrito_id" value="<?php echo $carito['carrito_id']; ?>" />
+                                            <button type="submit" style="color:white;background: goldenrod;">-</button>
+                                        </form>
+                                        <form action="carrito.php" method="post">
+                                            <input type="hidden" name="action" value="mas" />
+                                            <input type="hidden" name="page" value="<?php echo ($_GET['page']) ?>" />
+                                            <input type="hidden" name="carrito_id" value="<?php echo $carito['carrito_id']; ?>" />
+                                            <button type="submit" style="color:white;background: goldenrod;">+</button>
+                                        </form>
+                                    </div>
+                                </td>
+                                <td><?php 
+                                $parcial= ((int)$carito['cantidad'] * (float)$carito['precio_producto']);
+                                $total= $total + $parcial;
+                                echo ($parcial); ?></td>
+                                <td>
+                                    <form action="carrito.php" method="post">
+                                        <input type="hidden" name="action" value="delete" />
+                                        <input type="hidden" name="page" value="<?php echo ($_GET['page']) ?>" />
+                                        <input type="hidden" name="carrito_id" value="<?php echo $carito['carrito_id']; ?>" />
+                                        <button type="submit" class="bi bi-trash"></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
-                </Table>                                   
+                </Table>
             </div>
             <div class="modal-footer d-flex justify-content-between">
                 <tr>
-                    <td><button type="button" class="btn btn-danger link-light" data-bs-dismiss="modal" style="padding: .25rem 1rem;
-                    border-radius: 4px; color: white; font-weight: bold; text-align: center;">Vaciar Todo</button></td>
+                    <td>
+                    <form action="carrito.php" method="post">
+                        <input type="hidden" name="action" value="vaciar" />
+                        <input type="hidden" name="page" value="<?php echo ($_GET['page']) ?>" />
+                        <button type="submit" class="btn btn-danger link-light" data-bs-dismiss="modal" style="padding: .25rem 1rem;
+                    border-radius: 4px; color: white; font-weight: bold; text-align: center;">Vaciar Todo</button>
+                    </form>
+                    </td>
 
-                    <p class="link-light">Total Productos: 3</p>
-                                                   
-                    <p class="link-light">Total: (cantidad)Bs.</p>
+                    <p class="link-light">Total Productos: <?php echo($contadorCarrito?$contadorCarrito:0) ?></p>
 
-                    <td><button type="button" class="btn btn-success link-light" data-bs-dismiss="modal" style="padding: .25rem 1rem;border-radius: 6px; color: white; font-weight: bold;
-                    text-align: center;">Comprar</button></td>
+                    <p class="link-light">Total: <?php echo($total) ?> Bs.</p>
+
+                    <td>
+                    <form action="carrito.php" method="post">
+                        <input type="hidden" name="action" value="comprar" />
+                        <input type="hidden" name="page" value="<?php echo ($_GET['page']) ?>" />
+                        <button type="submit" class="btn btn-success link-light" data-bs-dismiss="modal" style="padding: .25rem 1rem;border-radius: 4px; color: white; font-weight: bold;
+                    text-align: center;">Comprar</button>
+                    </form>
+                    </td>
                 </tr>
             </div>
         </div>
     </div>
-</div>  
-                                  
-<!-- fin cuerpo modal -->
+</div>
+
