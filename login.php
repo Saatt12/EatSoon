b7857b8eb69f2f5ed8775f90b6ee5873cbcc2b9c
EDelgadillo <104334841+EDelgadillo26@users.noreply.github.com>
2022-12-02 21:35:07 -0400

    Index con logeo

--- login.php
+++ login.php
@@ -1,20 +1,36 @@
 <?php
-
+error_reporting(E_ALL ^ E_NOTICE);
+    require './functions/session.php';
     require './config/env.php';
     require './config/conexion.php';
     require './functions/loginf.php';
     require './functions/file.php';
-
+if (!$_SESSION){
+    iniSesion();
+}
     $con = conexion($db_config);
 
     /** Verificar si se envio por el metodo POST */
     if($_SERVER['REQUEST_METHOD'] == 'POST'){    
         $data = [
-            'usuario' => $_POST['usuario'],
+            'correo' => $_POST['correo'],
             'password' => $_POST['password']
         ];
 
-        loginUser($con, $data);
+        $user=loginUser($con, $data);
+        if ($user!=null){
+            iniSesion();
+            session__set("user",$user['CORREO']);
+            ob_start();
+            $url = RUTA.'/index_usuario_creado.php';
+            while (ob_get_status())
+            {
+                ob_end_clean();
+            }
+            //header( "Location: $url" );
+            echo "<script>window.location = '$url'</script>";
+            exit();
+        }
     }
     
     
@@ -22,4 +38,5 @@
 
     $page = './pages/login.page.php';  // Nombre y ruta de la pagina.
     require './templates/login.template.php'; // Require template
+exit();
 ?>
